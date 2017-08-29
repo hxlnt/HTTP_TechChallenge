4283ee0982e6c0d0f46a72b5c148377a61c79961
Rachel Simone Weil <partytimehexcellent@gmail.com>
2017-08-29 12:30:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var fetch = require('node-fetch')
 
-const token = 'ENTER YOUR AUTH TOKEN HERE'
+const token = 'ENTER YOUR AUTH TOKEN HERE';
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let myTimer = setTimeout(checkOnTimer, 3000);
 var thisurl;
 
-function checkOnTimer() {
     for (j = 0; j < 26; j++) {
         for (i = 0; i < 26; i++) {
             thisurl = letters[i] + letters[j];
@@ -20,5 +18,3 @@ function checkOnTimer() {
                 });
         }
     }
-    clearTimeout(myTimer);
-}
